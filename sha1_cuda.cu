00000000c50369e51bb9896d2004a3f9aafb041f
ziguips <zigui_@naver.com>
2019-06-04 21:56:23 +0900

    v1.0.0                                       DCEBM@@G@@@@@@@@

--- sha1_cuda.cu
+++ sha1_cuda.cu
@@ -11,16 +11,16 @@ __device__ uint LR_dev(uint a, int x){
 	return a << x | a >> 32-x;
 }
 
-__global__ void sha1_kernel(ull* res, ull IDX, uint h0, uint h1, uint h2, uint h3, uint h4) {
-	ull id = threadIdx.x | (ull)blockIdx.x << 10 | (ull)blockIdx.y << 20 | (ull)IDX << 32, idx = id;
+__global__ void sha1_kernel(ull* res, ull IDX, uint h0, uint h1, uint h2, uint h3, uint h4, uint nbits) {
+	ull id = threadIdx.x | (ull)blockIdx.x << 8 | (ull)blockIdx.y << 20 | (ull)IDX << 32, idx = id;
 
 	uint w[16];
 	for(int i = 0; i < PADDING/4; i++, idx >>= 16){
 		w[i] = 0x40404040u | (idx&15) << 24 | (idx>>4&15) << 16 | (idx>>8&15) << 8 | (idx>>12&15);
 	}
 	w[PADDING/4] = 0x0a800000;
-	for(int i = PADDING/4; i < 15; i++) w[i] = 0;
-	w[15] = 1800;
+	for(int i = PADDING/4+1; i < 15; i++) w[i] = 0;
+	w[15] = nbits;
 
 	uint a, b, c, d, e, f, k;
 	a = h0; b = h1; c = h2; d = h3; e = h4;
@@ -71,34 +71,43 @@ __global__ void sha1_kernel(ull* res, ull IDX, uint h0, uint h1, uint h2, uint h
 	h2 = h2 + c;
 	h3 = h3 + d;
 	h4 = h4 + e;
-	if(h0 == 0 && (h1 >> 31) == 0) *res = id;
+//	if(h0 == 0 && (h1 >> 20) == 0) *res = id;
+	if(h0 == 0) *res = id;
 }
 
 struct sha1{
-	sha1(uint h0, uint h1, uint h2, uint h3, uint h4){}
-	uint h0, h1, h2, h3, h4;
+	sha1(uint h0, uint h1, uint h2, uint h3, uint h4, uint nbits):
+		h0(h0), h1(h1), h2(h2), h3(h3), h4(h4), 
+		nbits(nbits){}
+	uint h0, h1, h2, h3, h4, nbits;
 };
 
 string modify_message()
 {
 	string log = "";
 	char c;
-	while(scanf("%c", &c) != EOF) log += c;
-	while(log.size() % 64 == 0) log += " ";
+
+	while(scanf("%c", &c) != EOF) log += c; log.pop_back();
+
+	while(log.size() % 64 != 0) log += " ";
 	for(int t = 0; t < PADDING; t++) log += "@";
-	log += '\0';
-
-	int m = 1, l = log.size(), p;
-	while(log.size() >= m*10) m *= 10;
+	
+	log += "\n";
+	
+	int l = log.size()-1;
+	for(int i = 0; i < log.size() && log[i]; i++) l--;
+	
+	log += "\x0";
+	
+	int m = 1, p;
+	while(l >= m*10) m *= 10;
 	for(p = 7; m; p++, m /= 10) log[p] = l/m + '0', l %= m;
-	cout << log << std::endl;
-
+	
 	if(log[p] != '\0'){
 		printf("message length's digit is changed while modifying\n");
 		printf("Please add or remove +- 50? letters \n");
 		exit(-1);
 	}
-	cout << log << "\n";
 	return log;
 }
 
@@ -107,13 +116,12 @@ union block{
 	char s[64];
 };
 
-uint h0, h1, h2, h3, h4;
-
 uint LR(uint a, int x){
 	return a << x | a >> 32-x;
 }
 
 sha1 get_hash(string log){
+	uint h0, h1, h2, h3, h4;
 	block buf[10000] = {};
 	h0 = 0x67452301;
 	h1 = 0xEFCDAB89;
@@ -121,7 +129,7 @@ sha1 get_hash(string log){
 	h3 = 0x10325476;
 	h4 = 0xC3D2E1F0;
 
-	memcpy(buf, log.c_str(), log.size()*8);
+	memcpy(buf, log.c_str(), log.size());
 	int len = log.size(), nbits = len * 8;
 	buf[0].s[len++] = 0x80;
 
@@ -168,7 +176,7 @@ sha1 get_hash(string log){
 		h3 = h3 + d;
 		h4 = h4 + e;
 	}
-	return sha1(h0, h1, h2, h3, h4);
+	return sha1(h0, h1, h2, h3, h4, nbits);
 }
 
 void run_on_gpu(string log, sha1 hash){
@@ -180,12 +188,12 @@ void run_on_gpu(string log, sha1 hash){
 	cudaMalloc(&res, sizeof(ull));
 
 	for(int i = 0;; i++){
-		sha1_kernel<<<numBlocks, threadsPerBlock>>>(res, i, hash.h0, hash.h1, hash.h2, hash.h3, hash.h4);
+		sha1_kernel<<<numBlocks, threadsPerBlock>>>(res, i, hash.h0, hash.h1, hash.h2, hash.h3, hash.h4, hash.nbits);
 
 		cudaMemcpy(&res_copy, res, sizeof(ull), cudaMemcpyDeviceToHost);
 		if(res_copy) break;
 	}
-	cout << log.substr(0, (int)log.size() - PADDING);
+	cout << log.substr(0, (int)log.size() - PADDING-1);
 	for(int i = 0; i < PADDING; i++){
 		printf("%c", 64 | res_copy&15);
 		res_copy /= 16;
